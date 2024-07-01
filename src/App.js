5bb5991806787dde1fc26b58efff249668add2d7
paulavanina <bejera_paula@yahoo.com>
2024-07-01 17:29:53 +0300

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,36 +1,23 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import './App.css';
 import Card from './Card';
 
-
 const apiURL = 'https://rickandmortyapi.com/api/character';
 
 function App() {
+  const [characters, setCharacters] = useState([]);
 
-  const character1={
-    "id": 8,
-    "name": "Adjudicator Rick",
-    "status": "Dead",
-    "species": "Human",
-    "type": "",
-    "gender": "Male",
-    "origin": {
-        "name": "unknown",
-        "url": ""
-    },
-    "location": {
-        "name": "Citadel of Ricks",
-        "url": "https://rickandmortyapi.com/api/location/3"
-    },
-    "image": "https://rickandmortyapi.com/api/character/avatar/8.jpeg",
-    "episode": [
-        "https://rickandmortyapi.com/api/episode/28"
-    ],
-    "url": "https://rickandmortyapi.com/api/character/8",
-    "created": "2017-11-04T20:03:34.737Z"
-}
+  const searchCharacters = async (name) => {
+    const response = await fetch(`${apiURL}?name=${name}`);
+    const data = await response.json();
+    setCharacters(data.results);
+  };
+
+  useEffect(() => {
+    searchCharacters('Rick');
+  }, []);
 
   return (
     <div className="App">
@@ -38,26 +25,32 @@ function App() {
         Rick and Morty <span className='text-primary' style={{ color: 'blue' }}>App</span>
       </h1>
       
-    <div className="container">
-      <Card character1={character1} />
-    </div>
+      <div className="container">
+        <div className="row">
+          {characters.map((character) => (
+            <div className="col-md-4" key={character.id}>
+              <Card character={character} />
+            </div>
+          ))}
+        </div>
+      </div>
 
-<div className="paginationClass">
-      <nav aria-label="...">
-        <ul className="pagination">
-          <li className="page-item disabled">
-            <span className="page-link">Previous</span>
-          </li>
-          <li className="page-item"><a className="page-link" href="#">1</a></li>
-          <li className="page-item active" aria-current="page">
-            <span className="page-link">2</span>
-          </li>
-          <li className="page-item"><a className="page-link" href="#">3</a></li>
-          <li className="page-item">
-            <a className="page-link" href="#">Next</a>
-          </li>
-        </ul>
-      </nav>
+      <div className="paginationClass">
+        <nav aria-label="...">
+          <ul className="pagination">
+            <li className="page-item disabled">
+              <span className="page-link">Previous</span>
+            </li>
+            <li className="page-item"><a className="page-link" href="#">1</a></li>
+            <li className="page-item active" aria-current="page">
+              <span className="page-link">2</span>
+            </li>
+            <li className="page-item"><a className="page-link" href="#">3</a></li>
+            <li className="page-item">
+              <a className="page-link" href="#">Next</a>
+            </li>
+          </ul>
+        </nav>
       </div>
     </div>
   );
